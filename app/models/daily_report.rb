f110fadd2ccee48eae989bf9a0377ecabf0ebd05
mkrms <new.fountain@docomo.ne.jp>
2021-05-21 20:42:19 +0900

    delete validate

--- app/models/daily_report.rb
+++ app/models/daily_report.rb
@@ -1,5 +1,5 @@
 class DailyReport < ApplicationRecord
-validates :title, presence: true
-validates :body, presence: true
-validates :team, presence: true
+#validates :title, presence: true
+#validates :body, presence: true
+#validates :team, presence: true
 end
